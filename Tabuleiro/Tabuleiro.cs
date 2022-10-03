3b12dc91f17462aabfb4c4880a2ce8f0d261e275
Guilherme Siviero <guilherme.psna@gmail.com>
2022-10-03 13:32:56 -0300

    Comecando a implementar o tabuleiro

---
+++ Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int Linhas, int Colunas)
+        {
+            linhas = Linhas;
+            colunas = Colunas;
+            pecas = new Peca[Linhas, Colunas];
+        }
+    }
+}
