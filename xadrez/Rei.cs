4c41a689108abbcf5c714e035d6246011c7da3dc
Guilherme Siviero <guilherme.psna@gmail.com>
2022-10-03 14:47:30 -0300

    Comecando a colocando pecas no tabuleiro

---
+++ xadrez/Rei.cs
@@ -0,0 +1,20 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    internal class Rei : Peca
+    {
+        public Tabuleiro Tab { get; set; }
+        public Cor Cor { get; set; }
+        public Rei(Tabuleiro tab, Cor cor) : base (tab, cor)
+        {
+            Tab = tab;
+            Cor = cor;
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
---
+++ xadrez/Torre.cs
@@ -0,0 +1,20 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    internal class Torre : Peca
+    {
+        public Tabuleiro Tab { get; set; }
+        public Cor Cor { get; set; }
+        public Torre(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+            Tab = tab;
+            Cor = cor;
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
